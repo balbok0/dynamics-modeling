ddeee15544cdf860e9c82ae3644dc1a8ca51e276
Jakub Filipek <balbok@cs.washington.edu>
2022-03-27 23:19:37 -0700

    add filters

---
+++ src/data_utils/bag_processing/filters/forward_filter.py
@@ -0,0 +1,32 @@
+from genpy import Duration
+from rospy import AnyMsg, Time
+
+from data_utils.bag_processing.msg_stubs import PIDInfo
+from .abstract_filter import AbstractFilter
+
+class ForwardFilter(AbstractFilter):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Initialize per-bag variables
+        self.end_bag()
+
+        self.after_last_threshold_log = Duration(1)  # seconds
+
+        self.topics = [{'/{robot_name}/pid_info'}]
+
+    @property
+    def should_log(self) -> bool:
+        return self.cur_state
+
+    def callback(self, msg: PIDInfo, ts: Time, topic: str):
+        self.cur_state = (
+            msg.vel > 1e-6 or (ts - self.last_forward_msg) < self.after_last_threshold_log
+        )
+
+        if msg.vel > 1e-6:
+            self.last_forward_msg = ts
+
+    def end_bag(self):
+        self.cur_state = False
+        self.last_forward_msg = Time()
