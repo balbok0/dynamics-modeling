cbb2237dc35574c960f208664708b8829d372f9c
Jakub Filipek <balbok@cs.washington.edu>
2022-04-11 09:56:57 -0700

    Make pip install . work with -e flag

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
-
+import os
 from setuptools import find_packages
 
-print(find_packages("src/package/rosbag2torch"))
+# This is necessary to lookup pip with pip -e file. It also makes package_dir unnecessary
+os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src", "package"))
 
 setup(
     name='rosbag2torch',
@@ -11,8 +12,8 @@ setup(
     author='Jakub Filipek',
     author_email='balbok@cs.washington.edu',
     # packages=['rosbag2torch', 'rosbag2torch.*'],
-    packages=find_packages("src/package"),
-    package_dir={'rosbag2torch': 'src/package/rosbag2torch'},
+    packages=find_packages("."),
+    # package_dir={'rosbag2torch': 'src/package/rosbag2torch'},
     install_requires=[
         "numpy",
         "rospy-all",
